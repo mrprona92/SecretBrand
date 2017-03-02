14276ef2461001bbb05c7ac9469019c394da6d4d
Tran Hoa Binh <binh.tran.hoa@sofiamedix.vn>
2017-03-02 17:02:51 +0700

    [None] Update new version source code

--- app/src/main/java/com/mrprona/dota2assitant/base/util/UpdateUtils.java
+++ app/src/main/java/com/mrprona/dota2assitant/base/util/UpdateUtils.java
@@ -1,18 +1,25 @@
 package com.mrprona.dota2assitant.base.util;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Pair;
 
 import com.mrprona.dota2assitant.BeanContainer;
 import com.mrprona.dota2assitant.R;
 import com.mrprona.dota2assitant.base.activity.BaseActivity;
+import com.mrprona.dota2assitant.base.activity.ListHolderActivity;
 import com.mrprona.dota2assitant.base.remote.BaseRemoteServiceImpl;
 import com.mrprona.dota2assitant.base.service.update.UpdateService;
 
@@ -20,7 +27,10 @@ import com.mrprona.dota2assitant.base.service.update.UpdateService;
  * Created by Badr on 17.02.2015.
  */
 public class UpdateUtils {
+
+    private static Context mContext;
     public static void checkNewVersion(final Context context,final boolean userInitiative) {
+        mContext=context;
         if(BaseRemoteServiceImpl.isNetworkAvailable(context))
         {
             ProgressTask<Pair<Boolean,String>> task=new ProgressTask<Pair<Boolean, String>>() {
@@ -67,7 +77,13 @@ public class UpdateUtils {
                                         }
                                     };
                                     context.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-                                    enqueueId = updateService.loadNewVersion(context);
+
+
+
+                                    if(checkPermission()){
+                                        enqueueId = updateService.loadNewVersion(context);
+                                    }
+
                                     dialog.dismiss();
                                 }
                             },new DialogInterface.OnClickListener() {
@@ -110,6 +126,40 @@ public class UpdateUtils {
             else {
                 new LoaderProgressTask<Pair<Boolean,String>>(task,null).execute();
             }
+        }
+    }
+
+
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
+    public static boolean checkPermission()
+    {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if(currentAPIVersion>=android.os.Build.VERSION_CODES.M)
+        {
+            if (ContextCompat.checkSelfPermission(mContext, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) ListHolderActivity.getAppContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(mContext);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("To download a file it is necessary to allow required permission");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((Activity)mContext, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                        }
+                    });
+                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
+                    alert.show();
+                } else {
+                    ActivityCompat.requestPermissions((Activity)mContext, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
         }
     }
 
